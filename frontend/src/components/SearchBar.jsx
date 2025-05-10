88ca76a1633afb8ecd5ad81f1b20fee334656d1c
Nikhil Singh <nikhilbdps1256@gmail.com>
2025-05-11 02:27:22 +0530

    commit changes in search bar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -25,16 +25,22 @@ const SearchBar = () => {
   return showSearch && visible ? (
     <div
       className={`${
-        theme === "dark" ? "bg-black text-white" : "bg-gray-50 text-black"
-      }border-t border-b bg-gray-50 text-center`}
+        theme === "dark" ? "bg-black" : "bg-gray-50"
+      } border-t border-b text-center`}
     >
-      <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
+      <div
+        className={`inline-flex items-center justify-center border ${
+          theme === "dark" ? "border-gray-600" : "border-gray-400"
+        } px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2 ${
+          theme === "dark" ? "bg-gray-800" : "bg-white"
+        }`}
+      >
         <input
           type="text"
           placeholder="search"
           className={`flex-1 outline-none bg-inherit text-sm ${
-            theme === "dark" ? "text-white" : "text-black"
-          }`}
+            theme === "dark" ? "text-gray-300" : "text-black"
+          } placeholder-gray-500`}
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
